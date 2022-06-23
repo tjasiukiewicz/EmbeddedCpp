5d085b0632c7137a28d9605be0ffcec8071c5aa6
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2022-06-23 15:54:43 +0200

    Add static polimorph example

---
+++ static_polimorph/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+template<typename T>
+struct Instances {
+	Instances() {
+		Instances<T>::instances.push_back(static_cast<T*>(this));
+	}
+
+	static std::vector<T*> get_instances() {
+		return Instances<T>::instances;
+	}
+
+	~Instances() {
+		auto & inst = Instances<T>::instances;
+		inst.erase(std::remove(inst.begin(), inst.end(), this), inst.end());
+	}
+private:
+	static std::vector<T*> instances;
+};
+
+template<typename T>
+std::vector<T*> Instances<T>::instances;
+
+struct Airplane : public Instances<Airplane> {
+	Airplane(const std::string& name)
+		: name{name} {
+	}
+
+	const std::string & get_name() const {
+		return name;
+	}
+
+private:
+	std::string name;
+};
+
+int main() {
+	auto a1 = Airplane("F16");
+	auto a2 = Airplane("F22");
+	auto a3 = Airplane("DC8");
+
+	for (const auto & a: {&a1, &a2, &a3}) {
+		std::cout << a->get_name() << '\n';
+	}
+	
+	std::cout << "-------\n";
+
+	for (const auto & a: Airplane::get_instances()) {
+		std::cout << a->get_name() << '\n';
+	}
+
+	std::cout << "-------\n";
+
+	for (const auto & a: Instances<Airplane>::get_instances()) {
+		std::cout << a->get_name() << '\n';
+	}
+
+	{
+		auto a3 = Airplane("SZD Pirat");
+	}
+
+	std::cout << "-------\n";
+
+	for (const auto & a: a1.get_instances()) {
+		std::cout << a->get_name() << '\n';
+	}
+
+}
