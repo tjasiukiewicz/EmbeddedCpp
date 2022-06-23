2e1e739e0cd9f400ad1196d6d7b996dd9c85d9a1
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2022-06-23 12:02:04 +0200

    Add meta fibonacci example

--- meta/main.cpp
+++ meta/main.cpp
@@ -18,10 +18,25 @@ struct fractional<0> {
 	constexpr static int value = 1;
 };
 
+template<int Index>
+struct fibonacci {
+	constexpr static int value = fibonacci<Index - 2>::value + fibonacci<Index - 1>::value;
+};
+
+template<>
+struct fibonacci<1> {
+	constexpr static int value = 1;
+};
+
+template<>
+struct fibonacci<0> {
+	constexpr static int value = 1;
+};
+
 int main() {
 	std::cout << value2 << '\n';
 
-	std::cout << fractional<3>::value << '\n';
+	std::cout << "3! = " << fractional<3>::value << '\n';
 
-
+	std::cout << "fibonacci<5>::value = " << fibonacci<5>::value << '\n';
 }
